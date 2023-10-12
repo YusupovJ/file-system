2be1182264f698a752b177fd89d9994fada6766c
YusupovJ <jamshudanamana@gmail.com>
2023-10-12 20:44:47 +0500

    added create.js

---
+++ create.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const createFile = () => {
+	const fileName = "fresh.txt";
+	const url = `${__dirname}/files/${fileName}`;
+	const data = "I am fresh and young";
+	const options = {
+		flag: "wx",
+		encoding: "utf8",
+	};
+
+	fs.writeFile(url, data, options, (error) => {
+		if (error) console.log(error.message);
+		console.log("File succecfully created");
+	});
+};
+
+createFile();
